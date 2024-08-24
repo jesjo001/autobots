11853a05ff0628ca972a0ed20f10fe0ea88b9794
jesjo <automationlounge@gmail.com>
2024-08-24 17:36:39 +0100

    adding check before creating autobot

--- services/AutobotService.js
+++ services/AutobotService.js
@@ -10,32 +10,34 @@ export const createAutobots = async () => {
     const userData = userResponse.data[i];
 
     console.log('creating autobots', userData)
-    const autobot = await Autobot.create({
-      username: userData.username,
-      email: userData.email,
-    });
-
-    for (let j = 0; j < 10; j++) {
-      const postResponse = await axios.get("https://jsonplaceholder.typicode.com/posts");
-      const postData = postResponse.data[j];
-
-      const post = await Post.create({
-        title: `${postData.title}-${i}-${j}`,
-        body: postData.body,
-        autobotId: autobot.id,
-      });
-    console.log("creating autobot's post")
-
-      for (let k = 0; k < 10; k++) {
-        const commentResponse = await axios.get("https://jsonplaceholder.typicode.com/comments");
-        const commentData = commentResponse.data[k];
-        console.log("creating autobot's comments: ", commentData)
-
-        await Comment.create({
-          body: commentData.body,
-          postId: post.id,
+    if(userData){
+        const autobot = await Autobot.create({
+            username: userData?.username + Date.now().toString(),
+            email: `${userData?.username}'s email: ` + userData.email,
         });
-      }
+      
+          for (let j = 0; j < 10; j++) {
+            const postResponse = await axios.get("https://jsonplaceholder.typicode.com/posts");
+            const postData = postResponse.data[j];
+            console.log("creating autobot's post")
+      
+            const post = await Post.create({
+              title: `${postData.title}-${i}-${j}`,
+              body: postData.body,
+              autobotId: autobot.id,
+            });
+      
+            for (let k = 0; k < 10; k++) {
+              const commentResponse = await axios.get("https://jsonplaceholder.typicode.com/comments");
+              const commentData = commentResponse.data[k];
+              console.log("creating autobot's comments: ")
+      
+              await Comment.create({
+                body: commentData.body,
+                postId: post.id,
+              });
+            }
+        }
     }
   }
 };
